04b679e9b6103d75c857b0841f6ef121b1b7bbcc
Flavia <flaviamatta90@gmail.com>
2020-10-07 16:59:11 +0200

    all

--- db.php
+++ db.php
@@ -2,46 +2,64 @@
 
 $database = [
   [
-    "title" => "New Jersey",
+    "title" => "NEW YERSEY",
     "author" => "Bon Jovi",
     "year" => 1988,
     "poster" => "https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg"
   ],
   [
-    "title" => "Live at Wembley 86",
+    "title" => "LIVE AT WEMBLEY 86",
     "author" => "Queen",
     "year" => 1992,
     "poster" => "https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg"
   ],
   [
-    "title" => "Brave new World",
-    "author" => "Iron Maiden",
-    "year" => 2000,
-    "poster" => "https://i.ebayimg.com/images/g/1GwAAOSwFytaKr7l/s-l300.jpg"
-  ],
-  [
-    "title" => "Ten's Summoner's Tales",
+    "title" => "TEN'S SUMMONER'S TALES",
     "author" => "Sting",
     "year" => 1993,
     "poster" => "https://images-na.ssl-images-amazon.com/images/I/411BQR6BHRL.jpg"
   ],
   [
-    "title" => "Steve Gadd band",
+    "title" => "STEVE GADD BAND",
     "author" => "Steve Gadd band",
     "year" => 2018,
     "poster" => "https://m.media-amazon.com/images/I/81UtLzBDoEL._SS500_.jpg"
   ],
   [
-    "title" => "Brave new World",
+    "title" => "BRAVE NEW WORLD",
     "author" => "Iron Maiden",
     "year" => 2000,
     "poster" => "https://i.ebayimg.com/images/g/1GwAAOSwFytaKr7l/s-l300.jpg"
   ],
   [
-    "title" => "One more car, one more rider",
+    "title" => "ONE MORE CAR, ONE MORE RIDER",
     "author" => "Eric Clapton",
     "year" => 2002,
     "poster" => "https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg"
+  ],
+  [
+    "title" => "MADE IN JAPAN",
+    "author" => "Deep Purple",
+    "year" => 1972,
+    "poster" => "https://images-na.ssl-images-amazon.com/images/I/51rggtPgmRL.jpg"
+  ],
+  [
+    "title" => "AND JUSTICE FOR ALL",
+    "author" => "Metallica",
+    "year" => 1988,
+    "poster" => "https://images-na.ssl-images-amazon.com/images/I/81r3FVfNG3L._SY355_.jpg"
+  ],
+  [
+    "title" => "HARD WIRED",
+    "author" => "Dave Weckl",
+    "year" => 1994,
+    "poster" => "https://img.discogs.com/KOBsqQwKiNKH-q927oHMyVdDzSo=/fit-in/596x596/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-6406665-1418464475-6120.jpeg.jpg"
+  ],
+  [
+    "title" => "BAD",
+    "author" => "Michael Jackson",
+    "year" => 1987,
+    "poster" => "https://m.media-amazon.com/images/I/71K9CbNZPsL._SS500_.jpg"
   ]
 ]
 ?>
