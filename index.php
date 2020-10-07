91c71401d05dc0bce12eca057c397497f114227a
Flavia <flaviamatta90@gmail.com>
2020-10-07 14:56:04 +0200

    first step

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php include "db.php"?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <div class="container">
+      <?php if(!empty($database)){?>
+        <?php foreach ($database as $databases) { ?>
+          <h2><?php echo $databases['title']; ?></h2>
+          <h3> <?php echo $databases["author"]?> </h3>
+          <h3> <?php echo $databases["year"]?> </h3>
+          <h3> <?php echo $databases["poster"]?> </h3>
+          <?php } ?>
+      <?php } else {
+        echo "Non ci sono album presenti";
+      }?>
+
+  </body>
+</html>
